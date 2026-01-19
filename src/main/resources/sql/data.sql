19d21b6ac64236cf70f696824c61a3d384c9e0d0
Warry <hei.warry@gmail.com>
2026-01-19 10:48:25 +0300

    chore: add new data

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,25 +1,21 @@
-insert into dish (id, name, dish_type)
-values (1, 'Salaide fraîche', 'STARTER'),
-       (2, 'Poulet grillé', 'MAIN'),
-       (3, 'Riz aux légumes', 'MAIN'),
-       (4, 'Gâteau au chocolat ', 'DESSERT'),
-       (5, 'Salade de fruits', 'DESSERT');
+insert into dish (id, name, dish_type, selling_price)
+    values (1, 'Salade fraîche', 'STARTER', 3500.00),
+           (2, 'Poulet grillé', 'MAIN', 12000.00),
+           (3, 'Riz aux légumes', 'MAIN', NULL),
+           (4, 'Gâteau au chocolat', 'DESSERT', 8000.00),
+           (5, 'Salade de fruits', 'DESSERT', NULL);
 
+insert into ingredient (id, name, price, category)
+    values (1, 'Laitue', 800.00, 'VEGETABLE'),
+           (2, 'Tomate', 600.00, 'VEGETABLE'),
+           (3, 'Poulet', 8000.00, 'ANIMAL'),
+           (4, 'Huile', 1500.00, 'OTHER'),
+           (5, 'Chocolat', 7000.00, 'OTHER'),
+           (6, 'Fruits assortis', 3000.00, 'VEGETABLE');
 
-insert into ingredient (id, name, category, price, id_dish)
-values (1, 'Laitue', 'VEGETABLE', 800.0, 1),
-       (2, 'Tomate', 'VEGETABLE', 600.0, 1),
-       (3, 'Poulet', 'ANIMAL', 4500.0, 2),
-       (4, 'Chocolat ', 'OTHER', 3000.0, 4),
-       (5, 'Beurre', 'DAIRY', 2500.0, 4);
-
-
-
-update dish
-set price = 2000.0
-where id = 1;
-
-update dish
-set price = 6000.0
-where id = 2;
-
+insert into dish_ingredient (id, id_dish, id_ingredient, quantity_required, unit)
+    values (1, 1, 1, 0.20, 'KG'),
+           (2, 1, 2, 0.15, 'KG'),
+           (3, 2, 3, 1.00, 'KG'),
+           (4, 2, 4, 0.30, 'L'),
+           (5, 4, 5, 0.20, 'KG');
