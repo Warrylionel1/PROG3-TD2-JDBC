ac781c311952d9ccdb3978704b805534c8f3a772
Warry <hei.warry@gmail.com>
2026-01-27 19:31:50 +0300

    feat: add DishIngredient class

--- src/main/java/DishIngredient.java
+++ src/main/java/DishIngredient.java
@@ -2,21 +2,24 @@ import java.util.Objects;
 
 public class DishIngredient {
     private Integer id;
-    private Integer dishId;
-    private Integer ingredientId;
+    private Ingredient ingredient;
     private Double quantityRequired;
-    private String unit;
+    private UnitEnum unit;
 
     public DishIngredient() {
     }
 
-    public DishIngredient(Integer id, Integer dishId, Integer ingredientId,
-                          Double quantityRequired, String unit) {
+    public DishIngredient(Integer id, Ingredient ingredient, Double quantityRequired, UnitEnum unit) {
         this.id = id;
-        this.dishId = dishId;
-        this.ingredientId = ingredientId;
+        this.ingredient = ingredient;
         this.quantityRequired = quantityRequired;
         this.unit = unit;
+    }
+
+    public DishIngredient(UnitEnum unit, Double quantityRequired, Ingredient ingredient) {
+        this.unit = unit;
+        this.quantityRequired = quantityRequired;
+        this.ingredient = ingredient;
     }
 
     public Integer getId() {
@@ -27,20 +30,12 @@ public class DishIngredient {
         this.id = id;
     }
 
-    public Integer getDishId() {
-        return dishId;
+    public Ingredient getIngredient() {
+        return ingredient;
     }
 
-    public void setDishId(Integer dishId) {
-        this.dishId = dishId;
-    }
-
-    public Integer getIngredientId() {
-        return ingredientId;
-    }
-
-    public void setIngredientId(Integer ingredientId) {
-        this.ingredientId = ingredientId;
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
     }
 
     public Double getQuantityRequired() {
@@ -51,11 +46,11 @@ public class DishIngredient {
         this.quantityRequired = quantityRequired;
     }
 
-    public String getUnit() {
+    public UnitEnum getUnit() {
         return unit;
     }
 
-    public void setUnit(String unit) {
+    public void setUnit(UnitEnum unit) {
         this.unit = unit;
     }
 
@@ -63,26 +58,21 @@ public class DishIngredient {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         DishIngredient that = (DishIngredient) o;
-        return Objects.equals(id, that.id) &&
-                Objects.equals(dishId, that.dishId) &&
-                Objects.equals(ingredientId, that.ingredientId) &&
-                Objects.equals(quantityRequired, that.quantityRequired) &&
-                Objects.equals(unit, that.unit);
+        return Objects.equals(id, that.id) && Objects.equals(ingredient, that.ingredient) && Objects.equals(quantityRequired, that.quantityRequired) && unit == that.unit;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, dishId, ingredientId, quantityRequired, unit);
+        return Objects.hash(id, ingredient, quantityRequired, unit);
     }
 
     @Override
     public String toString() {
         return "DishIngredient{" +
                 "id=" + id +
-                ", dishId=" + dishId +
-                ", ingredientId=" + ingredientId +
+                ", ingredient=" + ingredient +
                 ", quantityRequired=" + quantityRequired +
-                ", unit='" + unit + '\'' +
+                ", unit=" + unit +
                 '}';
     }
 }
